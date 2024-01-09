a01a75bbb39624ba4f71a3ab939e3d502453702d
Fouche du Preez <fouche@limehouse.co.za>
2024-01-09 12:35:26 +0200

    Implemented game

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,54 @@
 <script>
-  import './app.css'
+import './app.css'
+import Team from './Team.svelte'
+
+let winningText = ''
+let winningColor = ''
+let redScore = 20
+let blueScore = 20
+
+const onScoreSubmit = (e) => {
+  const {score, color} = e.detail
+  console.log('score:' + score + ' color:' + color)
+  if (score === 0 ) {
+    if (color.toUpperCase() === 'RED') {
+      winningText = 'Red wins!'
+      winningColor = 'text-red-500'
+    } else if (color.toUpperCase() === 'BLUE') {
+      winningText = 'Blue wins!'
+      winningColor = 'text-blue-500'    
+    }
+  }
+}
+
+const resetGame = () => {
+  winningText = ''
+  redScore = 20
+  blueScore = 20
+  console.log('resetting game')
+}
+
 </script>
 
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<div class="container mx-auto max-w-4xl">
+  <div class="grid grid-cols-2 py-4">
+    <div class="col-span-2 text-2xl font-bold mb-2 flex place-content-center">
+      MTG Counter App
+    </div>
+    <Team color='red' bind:score={redScore} on:score_submit={onScoreSubmit}/>
+    <Team color='blue' bind:score={blueScore} on:score_submit={onScoreSubmit}/>
+    {#if winningText === ''}    
+      <div class="col-span-2">
+        <button on:click={resetGame} class="w-full bg-yellow-500 font-bold mt-3 p-1 rounded-md">Reset Game</button>
+      </div>      
+    {:else}
+      <div class="flex place-content-center col-span-2 {winningColor} text-xl font-bold mt-2">
+        {winningText}
+      </div>    
+      <div class="col-span-2">
+        <button on:click={resetGame} class="w-full bg-green-800 text-white font-bold mt-3 p-1 rounded-md">New Game</button>
+      </div>      
+    {/if}
+  </div>
+</div>
 
---
+++ src/Team.svelte
@@ -0,0 +1,52 @@
+<script>
+export let color
+export let score
+
+let borderColor = ''
+let fontColor = ''
+let teamText = ''
+
+if (color.toUpperCase() === 'RED') {
+	borderColor = 'border-red-500'
+	fontColor = 'text-red-500'
+	teamText = 'Red'
+} else {
+	borderColor = 'border-blue-500'
+	fontColor = 'text-blue-500'
+	teamText = 'Blue'
+}
+
+const increaseScore = () => {
+	if (score >= 0 && score < 20) {
+		score++
+	}
+}
+
+const decreaseScore = () => {
+	if (score > 0 && score <= 20) {
+		score--
+	}
+}
+
+
+import {createEventDispatcher} from 'svelte'
+const dispatch = createEventDispatcher()
+const submitScore = () => {
+	dispatch('score_submit', {
+		score,
+		color
+	})
+}
+
+</script>
+<div>
+	<form on:submit|preventDefault={submitScore}>
+	<div class="border-4 {borderColor} border-dashed">
+		<div class="text-xl font-bold {fontColor} mt-3 flex place-content-center">Team {teamText}: {score}</div>
+		<div class="mb-3 flex place-content-center">
+			<div><button on:click={increaseScore} class="bg-green-500 w-8 h-8 mr-1 rounded-md font-extrabold text-white">+</button></div>
+			<div><button on:click={decreaseScore} class="bg-red-500 w-8 h-8 rounded-md font-extrabold text-white">-</button></div>
+		</div>        
+	</div>      
+	</form>
+</div>
